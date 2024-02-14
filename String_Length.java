f86fe1b3a3ea0038d49339115bbc06d08bbd5c36
22P31A05C8 <156403726+22P31A05C8@users.noreply.github.com>
2024-02-14 11:08:29 +0530

    import java.util.Scanner; public class Strings {     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         String f=sc.nextLine();         System.out.print(f.length());
        }
    }

---
+++ String_Length.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Strings
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        String f=sc.nextLine();
+        System.out.print(f.length());
+ 
+    }
+}
